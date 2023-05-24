cd32053df9525698a1d5dd8169ff782bf5ce60c8
RendyAFS <99011571+RendyAFS@users.noreply.github.com>
2023-05-24 14:25:07 +0700

    Update Mean Amplitudo.py

--- Mean Amplitudo.py
+++ Mean Amplitudo.py
@@ -10,9 +10,8 @@ s1_values = data["s1"].values
 # Menghitung mean jarak S1
 mean_s1_distance = sum(s1_values[i] - s1_values[i-1] for i in range(1, len(s1_values))) / (len(s1_values) - 1)
 
-print(f"Mean Jarak S1-S1:\n{sum(s1_values[i] - s1_values[i-1] for i in range(1, len(s1_values)))} / {len(s1_values) - 1} = {mean_s1_distance}")
+print(f"Mean Jarak S1-S1:{sum(s1_values[i] - s1_values[i-1] for i in range(1, len(s1_values)))} / {len(s1_values) - 1} = {mean_s1_distance}")
 
-print("\n")
 
 # MENCARI MEAN JARAK S2-S2
 import pandas as pd
@@ -26,10 +25,8 @@ s2_values = data["s2"].values
 # Menghitung mean jarak S1
 mean_s12_distance = sum(s2_values[i] - s2_values[i-1] for i in range(1, len(s2_values))) / (len(s2_values) - 1)
 
-print(f"Mean Jarak S2-S2:\n{sum(s2_values[i] - s2_values[i-1] for i in range(1, len(s2_values)))} / {len(s2_values) - 1} = {mean_s12_distance}")
+print(f"Mean Jarak S2-S2:{sum(s2_values[i] - s2_values[i-1] for i in range(1, len(s2_values)))} / {len(s2_values) - 1} = {mean_s12_distance}")
 
-
-print("\n")
 
 # MENCARI MEAN JARAK S1-S2
 import pandas as pd
@@ -44,4 +41,4 @@ s2_values = data["s2"].values
 # Menghitung mean jarak S1
 mean_s2_distance = sum(s2_values[i] - s1_values[i-1] for i in range(len(s2_values))) / (len(s2_values))
 
-print(f"Mean Jarak S1-S2:\n{sum(s2_values[i] - s1_values[i-1] for i in range(len(s2_values)))} / {len(s2_values)} = {mean_s2_distance}")
+print(f"Mean Jarak S1-S2:{sum(s2_values[i] - s1_values[i-1] for i in range(len(s2_values)))} / {len(s2_values)} = {mean_s2_distance}")
